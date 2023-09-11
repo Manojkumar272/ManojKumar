790f75df690d9274dec2fe464e4c31982da25072
Manojkumar272 <144663816+Manojkumar272@users.noreply.github.com>
2023-09-11 13:15:47 +0530

    Add files via upload

---
+++ Chalanges Unit 1/chalange1.2.py
@@ -0,0 +1,7 @@
+# online python compiler (interpreter) or run python online.
+# write python3 code in this online editor and run it.
+year = 2023
+if(year%4 == 0):
+  print("leap year")
+else:
+  print("Not leap year")
